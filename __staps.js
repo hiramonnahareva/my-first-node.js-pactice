3bd51c463bc605730010cfc57b5bce07d9ebafe8
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2022-04-26 20:23:44 +0600

    modify post data and add some septs

--- __staps.js
+++ __staps.js
@@ -1,4 +1,7 @@
 /**
+ * ---------------------
+ * Basic server setup
+ * ---------------------
  * 1. create a folder.
  * 2. open command line to that folder.
  * 3. npm init.
@@ -7,11 +10,22 @@
  * -----------------
  * For express/node server
  * ------------------
- * 6. require express
- * 7.  create app variable
- * 8. declare port
- * 9. set app.get ('/')
- * 10. listen to port
- * 11.node index.js
- * 12. check browser for that port
+ * 1. require express
+ * 2.  create app variable
+ * 3. declare port
+ * 4. set app.get ('/')
+ * 5. listen to port
+ * 6.node index.js
+ * 7. check browser for that port
+ * ------------------
+ * get
+ * -------------------
+ * 1. cors middleware
+ * npm cors init
+ * then const cors = require ('cors') ;
+ * 2. 
+ * ------------
+ * post 
+ * ----------------
+ * 1. app.use (express.json());
  */
--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require ('express') ;
 const app = express();
 const cors = require ('cors') ;
 const port = process.env.PORT || 5000;
-app.use (cors())
-app.use (express.json());
+app.use(cors())
+app.use(express.json());
 app.get('/', (req, res) =>{
     res.send ('Look my My code node change num!!!')
 });
@@ -16,19 +16,39 @@ const users = [
     {id: 5, name: 'malona', email: 'malona@gmail.com', phone: '017229988'},
     {id: 6, name: 'salma', email: 'salma@gmail.com', phone: '017229988'}
 ]
+/* app.get ('/users', (req, res)=> {
+    res.send (users);
+}) */
+
+
+// filter by search query parameter
 app.get('/users', (req, res)=> {
+    if (req.query.name){
+        const search = req.query.name.toLowerCase();
+        const matched = users.filter (user => user.name.toLowerCase().includes(search));
+        res.send(matched);
+    }
     res.send(users);
 })
+// filter by search id parameter
 app.get('/user/:id', (req, res)=> {
     console.log (res.params)
     const id = parseInt(req.params.id);
-    const user = users.find(user => user.id === id);
+    const user = users.find(u => u.id === id);
     res.send(user);
 });
+// app.post('/user', (req, res) => {
+//     console.log ('request', req.body);
+//     res.send ('post method success')
+// })
 app.post('/user', (req, res) => {
-    console.log ('request', req)
-    res.send ('post method success')
+    console.log ('request', req.body);
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    res.send (user);
 })
+
 app.listen(port , ()=> {
     console.log ('Lisening, my port' , port);
 })
